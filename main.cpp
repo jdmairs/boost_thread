d674283dc169bc44bc266479725538d8ce850204
John Mairs <John.D.Mairs@gmail.com>
2021-11-25 11:11:49 -0500

    link to boost thread chrono

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <boost/thread.hpp>
 #include <iostream>
 
+void countDownThread()
+{
+    for (int i=5; i>0; i--)
+    {
+        std::cout << "Counting down: " << i << "\n";
+        boost::this_thread::sleep_for(boost::chrono::seconds(1));
+    }
+}
 int main()
 {
     std::cout << "Main thread: " <<  boost::this_thread::get_id() << "\n";
