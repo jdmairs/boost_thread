6676c24526121d59ee354ed2c415b7f7e73ed0a2
John Mairs <John.D.Mairs@gmail.com>
2021-11-25 11:17:25 -0500

    final version works

--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@ void countDownThread()
 {
     for (int i=5; i>0; i--)
     {
-        std::cout << "Counting down: " << i << "\n";
+        std::cout << "Test thread: Counting down: " << i << "     " << boost::this_thread::get_id() << "\n";
         boost::this_thread::sleep_for(boost::chrono::seconds(1));
     }
 }
 int main()
 {
     std::cout << "Main thread: " <<  boost::this_thread::get_id() << "\n";
+    boost::thread t1{countDownThread};
+    std::cout << "Main thread: joining\t\t\t" <<  boost::this_thread::get_id() << "\n";
+    t1.join();
+    std::cout << "Main thread: joined\t\t\t" << boost::this_thread::get_id() << "\n";
 }
